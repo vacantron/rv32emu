512acf7d32b5bdd166e8af8112d8806d687cc77e
Vacantron Chen <e24081030@gs.ncku.edu.tw>
2024-08-18 17:39:38 +0800

    Fix incorrect script

--- .ci/riscv-toolchain-install.sh
+++ .ci/riscv-toolchain-install.sh
@@ -9,14 +9,14 @@ check_platform
 mkdir -p toolchain
 
 if [[ "$#" = 0 ]] || [[ "$1" != "riscv-collab" ]]; then
+    GCC_VER=14.2.0-1
+    TOOLCHAIN_REPO=https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack
+    TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/v${GCC_VER}/xpack-riscv-none-elf-gcc-${GCC_VER}-linux-x64.tar.gz
+else
     UBUNTU_VER=`lsb_release -r | cut -f2`
     GCC_VER=2024.04.12
     TOOLCHAIN_REPO=https://github.com/riscv-collab/riscv-gnu-toolchain
     TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/${GCC_VER}/riscv32-elf-ubuntu-${UBUNTU_VER}-gcc-nightly-${GCC_VER}-nightly.tar.gz
-else
-    GCC_VER=14.2.0-1
-    TOOLCHAIN_REPO=https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack
-    TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/v${GCC_VER}/xpack-riscv-none-elf-gcc-${GCC_VER}-linux-x64.tar.gz
 fi
 
 wget -q ${TOOLCHAIN_URL} -O- | tar -C toolchain --strip-components=1 -xz
